a73b0939d8ee9beb5dd88ee8f0c868a29cabf15d
Colin McDonnell <colinmcd94@gmail.com>
2026-06-14 22:50:26 -0700

    site: align Introducing Nub blog with homepage numbers; restructure install/pm/FAQ sections + move FAQ answers to docs

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { Bench } from '@/components/code';
+import { InstallTabs } from '@/components/install-tabs';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     Bench,
+    InstallTabs,
     ...components,
   };
 }
